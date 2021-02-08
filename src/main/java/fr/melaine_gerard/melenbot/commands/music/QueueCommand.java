8b874f1be16bbfeb3ae152934f9af8a6322869de
Melaine Gérard <pro@melaine-gerard.fr>
2021-02-08 23:46:00 +0100

    updated QueueCommand to reduce size of the message

--- src/main/java/fr/melaine_gerard/melenbot/commands/music/QueueCommand.java
+++ src/main/java/fr/melaine_gerard/melenbot/commands/music/QueueCommand.java
@@ -37,7 +37,7 @@ public class QueueCommand implements ICommand {
             return;
         }
 
-        final int trackCount = Math.min(queue.size(), 20);
+        final int trackCount = Math.min(queue.size(), 10);
         final List<AudioTrack> trackList = new ArrayList<>(queue);
         final MessageAction messageAction = channel.sendMessage("** File d'attente actuelle:**\n");
 
@@ -45,16 +45,13 @@ public class QueueCommand implements ICommand {
             final AudioTrack track = trackList.get(i);
             final AudioTrackInfo info = track.getInfo();
             messageAction.append("#")
-                    .append(String.valueOf(i + 1))
+		    .append(String.valueOf(i + 1))
                     .append(" `")
                     .append(info.title)
                     .append("` - `")
                     .append(info.author)
-                    .append("`\n[`")
-                    .append(formatTime(track.getDuration()))
-                    .append("`]\n");
-
-        }
+                    .append("`\n");                  
+	}
 
         if(trackList.size() > trackCount){
             messageAction.append("et `")
@@ -66,11 +63,4 @@ public class QueueCommand implements ICommand {
 
     }
 
-    private String formatTime(long timeInMillis) {
-        final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
-        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1);
-        final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
-
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-    }
 }
