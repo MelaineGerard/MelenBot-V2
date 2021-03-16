e886bbdc508e6c7b4b5afc99f22d4c79e83c8f13
Melaine Gérard <pro@melaine-gerard.fr>
2021-03-16 13:36:18 +0100

    Updated ServerInfo

--- src/main/java/fr/melaine_gerard/melenbot/commands/infos/ServerinfoCommand.java
+++ src/main/java/fr/melaine_gerard/melenbot/commands/infos/ServerinfoCommand.java
@@ -2,9 +2,15 @@ package fr.melaine_gerard.melenbot.commands.infos;
 
 import fr.melaine_gerard.melenbot.enumerations.Category;
 import fr.melaine_gerard.melenbot.interfaces.ICommand;
+import fr.melaine_gerard.melenbot.utils.EmbedUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 public class ServerinfoCommand implements ICommand {
     @Override
@@ -19,6 +25,19 @@ public class ServerinfoCommand implements ICommand {
 
     @Override
     public void handle(GuildMessageReceivedEvent event, List<String> args) {
-
+        Guild guild = event.getGuild();
+        EmbedBuilder eb = EmbedUtils.createEmbed(event.getJDA())
+                .setTitle("Quelques informations sur le serveur " + guild.getName())
+                .setThumbnail(guild.getIconUrl())
+                .addField("Nom :", guild.getName(), false);
+                if(guild.getOwner() != null) eb.addField("Owner :", guild.getOwner().getUser().getAsTag(), false);
+                eb.addField("Date de création :", guild.getTimeCreated().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME.localizedBy(Locale.FRANCE).withLocale(Locale.FRANCE)), false)
+                .addField("Membres :", String.valueOf(guild.getMemberCount()), false)
+                .addField("Salons :", String.valueOf(guild.getChannels().size()), false)
+                .addField("Roles :", String.valueOf(guild.getRoles().size()), false)
+                .addField("Région :", guild.getRegion().getName(), false)
+                .addField("Boosts :", String.valueOf(guild.getBoostCount()), false)
+                .addField("Région :", String.valueOf(guild.getBoostTier().getKey()), false);
+        event.getChannel().sendMessage(eb.build()).queue();
     }
 }
