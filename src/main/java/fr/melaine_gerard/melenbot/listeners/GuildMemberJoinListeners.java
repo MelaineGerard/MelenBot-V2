5168a58f4e98ed4da19d8b106244b12faf32d70e
Melaine Gérard <pro@melaine-gerard.fr>
2021-04-09 09:24:15 +0200

    Updated dependencies and GuildMemberJoinListeners

--- src/main/java/fr/melaine_gerard/melenbot/listeners/GuildMemberJoinListeners.java
+++ src/main/java/fr/melaine_gerard/melenbot/listeners/GuildMemberJoinListeners.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.Objects;
 
 public class GuildMemberJoinListeners extends ListenerAdapter {
 
@@ -20,11 +19,10 @@ public class GuildMemberJoinListeners extends ListenerAdapter {
                 .replaceAll("%tag%", event.getUser().getAsTag())
                 .replaceAll("%guild%", event.getGuild().getName())
                 .replaceAll("\\|", System.lineSeparator());
-        if(DatabaseUtils.getWelcomeChannel(event.getGuild().getId()) == null) return;
-        TextChannel channel = event.getJDA().getTextChannelById(Objects.requireNonNull(DatabaseUtils.getWelcomeChannel(event.getGuild().getId())));
-        System.out.println("nice");
+        String channelId = DatabaseUtils.getWelcomeChannel(event.getGuild().getId());
+        if (channelId == null) return;
+        TextChannel channel = event.getJDA().getTextChannelById(channelId);
         if (channel == null) return;
-        System.out.println("3st attempt");
         channel.sendMessage(EmbedUtils.createEmbed(event.getJDA()).setTitle("Nouveau membre").setDescription(welcomeMessage).build()).queue();
     }
 }
