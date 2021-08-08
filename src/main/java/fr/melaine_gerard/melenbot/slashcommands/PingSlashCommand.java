e8cb1a035d1a015e8163e99483c820d041e2dede
Melaine Gérard <pro@melaine-gerard.fr>
2021-08-08 20:44:44 +0200

    Updated PingSlashCommand

--- src/main/java/fr/melaine_gerard/melenbot/slashcommands/PingSlashCommand.java
+++ src/main/java/fr/melaine_gerard/melenbot/slashcommands/PingSlashCommand.java
@@ -4,6 +4,7 @@ import fr.melaine_gerard.melenbot.interfaces.ISlashCommand;
 import fr.melaine_gerard.melenbot.utils.EmbedUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class PingSlashCommand implements ISlashCommand {
     @Override
@@ -14,12 +15,11 @@ public class PingSlashCommand implements ISlashCommand {
     @Override
     public void handle(SlashCommandEvent event) {
         long time = System.currentTimeMillis();
-        event.reply("Pong!").setEphemeral(false).queue(v -> {
-            EmbedBuilder eb = EmbedUtils.createEmbed(event.getJDA())
-                    .setTitle("Pong !")
-                    .addField("Gateway Ping : ", String.format("%dms", event.getJDA().getGatewayPing()), false)
-                    .addField("Rest Ping : ", String.format("%sms", event.getJDA().getRestPing().complete()), false);
-            v.editOriginalEmbeds(eb.build()).queue();
-        });
+        InteractionHook interactionHook = event.reply("Pong!").setEphemeral(false).complete();
+        EmbedBuilder eb = EmbedUtils.createEmbed(event.getJDA())
+                .setTitle("Pong !")
+                .addField("Gateway Ping : ", String.format("%dms", event.getJDA().getGatewayPing()), false)
+                .addField("Rest Ping : ", String.format("%sms", event.getJDA().getRestPing().complete()), false);
+        interactionHook.editOriginalEmbeds(eb.build()).queue();
     }
 }
