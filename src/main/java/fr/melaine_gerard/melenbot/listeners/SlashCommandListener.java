93f3ca222a138e62b7dc2fc2146c531a6fbef87b
Melaine Gérard <pro@melaine-gerard.fr>
2021-08-08 12:34:38 +0200

    updated SlashCommandListener

--- src/main/java/fr/melaine_gerard/melenbot/listeners/SlashCommandListener.java
+++ src/main/java/fr/melaine_gerard/melenbot/listeners/SlashCommandListener.java
@@ -1,6 +1,5 @@
 package fr.melaine_gerard.melenbot.listeners;
 
-import fr.melaine_gerard.melenbot.managers.CommandManager;
 import fr.melaine_gerard.melenbot.managers.SlashCommandManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
@@ -10,10 +9,12 @@ import org.jetbrains.annotations.NotNull;
 public class SlashCommandListener extends ListenerAdapter {
     private final SlashCommandManager slashCommandManager;
 
-    public SlashCommandListener(JDA jda){
-         slashCommandManager = new SlashCommandManager();
-         slashCommandManager.registerSlashCommands(jda);
+    public SlashCommandListener(JDA jda) {
+        slashCommandManager = new SlashCommandManager();
+        System.out.println(slashCommandManager.getSlashCommand("ping").getDescription());
+        slashCommandManager.registerSlashCommands(jda);
     }
+
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         if (event.getUser().isBot()) return;
